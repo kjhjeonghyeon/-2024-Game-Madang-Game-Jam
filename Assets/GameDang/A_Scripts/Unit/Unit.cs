62edee820745a2870db69231d3f88982ddd7fed5
ptk5758 <ptk725739@gmail.com>
2024-11-22 16:54:04 +0900

    Unit direction property add

--- Assets/GameDang/A_Scripts/Unit/Unit.cs
+++ Assets/GameDang/A_Scripts/Unit/Unit.cs
@@ -3,26 +3,28 @@ using UnityEngine;
 
 public abstract class Unit : MonoBehaviour
 {
-    public static event Action<Unit> OnSpawned;
     /// <summary>
     /// 소유 타워
     /// </summary>
     public Tower Owner { get; private set; }
 
     /// <summary>
-    /// Player 가 스폰 시킨 Unit 인지.
+    /// 이동 방향 1 이면 오른쪽 -1 이면 왼쪽
     /// </summary>
-    private bool _isPlayer;
+    private int direction = 1;
 
     public void Init(Tower onwer)
     {
         Owner = onwer;
-        _isPlayer = TowerManager.PlayerTower == onwer;
+        direction = TowerManager.PlayerTower == onwer ? 1 : -1;
         OnSpawned?.Invoke(this);
     }
     private void FixedUpdate()
     {
-        Vector2 direction = _isPlayer ? Vector2.right : Vector2.left;        
-        transform.position += (Vector3) direction * Time.deltaTime * 5; // 5 이부분 speed 들어가야함 하드코딩임
+        transform.position += (Vector3) Vector2.right  * direction * Time.deltaTime; // 5 이부분 speed 들어가야함 하드코딩임
     }
+
+    # region 이벤트
+    public static event Action<Unit> OnSpawned;
+    # endregion
 }
