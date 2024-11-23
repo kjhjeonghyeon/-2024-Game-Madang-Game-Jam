467f6a42e96f5e98f2a69e23b898e56578d79337
ptk5758 <ptk725739@gmail.com>
2024-11-23 22:07:18 +0900

    크리티컬 특성

--- Assets/GameDang/A_Scripts/Util.cs
+++ Assets/GameDang/A_Scripts/Util.cs
@@ -3,6 +3,6 @@ using UnityEngine;
 public class Util
 {
     public static bool Dice(int value) {
-        return Random.Range(0, 101) >= value;
+        return Random.Range(0, 101) >= 100 - value;
     }    
 }
