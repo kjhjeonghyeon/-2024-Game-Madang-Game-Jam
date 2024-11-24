dc766ee1a1b3f965eebd94c0c55f8fcb60aae1d5
ptk5758 <ptk725739@gmail.com>
2024-11-24 21:57:21 +0900

    Sound Manager add

---
+++ Assets/GameDang/A_Scripts/Manager/SoundManager.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SoundType
+{
+    Steampunk = 0,
+    Korea = 1,
+    Fantasy = 2,
+    Punk = 3
+}
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] private List<AudioClip> bgms;
+    [SerializeField] private AudioSource audioSource; // AudioSource 컴포넌트
+
+    public static SoundManager Instance
+    {
+        get => instance;
+        set => instance = value;
+    }
+    private static SoundManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void OnEnable()
+    {
+        Play(TowerManager.PlayerSelectTowerIndex);
+    }
+    private void OnDisable()
+    {
+        Stop();
+    }
+
+    public void Play(SoundType type) => Play((int) type);
+    public void Play(int index)
+    {
+        audioSource.clip = bgms[index];
+        audioSource.loop = true;
+        audioSource.Play();
+    }
+    public void Stop()
+    {
+        if (audioSource != null && audioSource.isPlaying)
+        {
+            audioSource.Stop();
+        }
+    }
+}
