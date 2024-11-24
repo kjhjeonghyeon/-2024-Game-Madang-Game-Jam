0ff10282c61433fdd1961a5202222df57e38c541
kjhjeonghyeon <80999210+kjhjeonghyeon@users.noreply.github.com>
2024-11-24 23:34:32 +0900

    페이지 넘기기

--- Assets/GameDang/A_Scripts/Manager/GameManager.cs
+++ Assets/GameDang/A_Scripts/Manager/GameManager.cs
@@ -1,13 +1,32 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    string GameScene;
     public void NewTowerSelect()
     {
         if (TowerManager.PlayerSelectTowerIndex != -1)
         {
             SceneManager.LoadScene("Battle");
         }
+    }
+
+    public void StartScene()
+    {
+
+        GameScene = "Start";
+        Invoke("SlowScene", 0.9f);
+    }
+    public void SelsectScene()
+    {
+
+        GameScene = "Select";
+        Invoke("SlowScene", 0.3f);
+    }
+    void SlowScene()
+    {
+        SceneManager.LoadScene(GameScene);
     }
 }
