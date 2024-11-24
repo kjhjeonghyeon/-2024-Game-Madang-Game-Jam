0a3cd84a491fee97b9974a46e8fccaca1e709051
kjhjeonghyeon <80999210+kjhjeonghyeon@users.noreply.github.com>
2024-11-24 19:51:16 +0900

    씬병합

--- Assets/GameDang/A_Scripts/Manager/GameManager.cs
+++ Assets/GameDang/A_Scripts/Manager/GameManager.cs
@@ -7,7 +7,7 @@ public class GameManager : MonoBehaviour
     {
         if (TowerManager.PlayerSelectTowerIndex != -1)
         {
-            SceneManager.LoadScene("Battle _Kim");
+            SceneManager.LoadScene("Battle");
         }
     }
 }
